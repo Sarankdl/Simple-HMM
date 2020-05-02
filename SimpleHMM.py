f49195a7125c9c0ffd322a707e5f1c40a473fd42
SaRaNkdl <sara.nikdl@gmail.com>
2020-05-02 15:24:29 -0700

    Add comments for building a simple HMM

--- SimpleHMM.py
+++ SimpleHMM.py
@@ -1,8 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from helpers import show_model
 from pomegranate import State, HiddenMarkovModel, DiscreteDistribution
 
 model = HiddenMarkovModel(name="Simple Model")
+
+#	 yes 	 no
+#Sunny 	0.10	0.90
+#Rainy 	0.80	0.20
 
 # emission probability distributions, P(umbrella | weather)
 sunny_emissions = DiscreteDistribution({"yes": 0.1, "no": 0.9})
@@ -13,15 +18,25 @@ rainy_state = State(rainy_emissions, name="Rainy")
 
 model.add_states(sunny_state, rainy_state)
 
+# create edges for each possible state transition in the model
+# equal probability of a sequence starting on either a rainy or sunny day
 model.add_transition(model.start, sunny_state, 0.5)
 model.add_transition(model.start, rainy_state, 0.5)
 
-model.add_transition(sunny_state, sunny_state, 0.8)
-model.add_transition(sunny_state, rainy_state, 0.2)
+#	 Sunny 	 Rainy
+#Sunny 	0.80	0.20
+#Rainy 	0.40	0.60
 
-model.add_transition(rainy_state, sunny_state, 0.4)
-model.add_transition(rainy_state, rainy_state, 0.6)
+model.add_transition(sunny_state, sunny_state, 0.8)  # 80% sunny->sunny
+model.add_transition(sunny_state, rainy_state, 0.2)  # 20% sunny->rainy
+
+model.add_transition(rainy_state, sunny_state, 0.4)  # 40% rainy->sunny
+model.add_transition(rainy_state, rainy_state, 0.6)  # 60% rainy->rainy
 
 model.bake()
+
+assert model.edge_count() == 6, "There should be two edges from model.start, two from Rainy, and two from Sunny"
+assert model.node_count() == 4, "The states should include model.start, model.end, Rainy, and Sunny"
+print("Great! You've finished the model.")
 
 print(model.dense_transition_matrix())
